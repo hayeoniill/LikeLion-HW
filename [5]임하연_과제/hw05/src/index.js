e9c9759885f3c5f6aad2474e8e0b01da04e9ddb3
hayeoniill <lhy5441@naver.com>
2025-04-10 17:45:04 +0900

    hw5

---
+++ [5]임하연_과제/hw05/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import ReactDOM from 'react-dom'; //구버전
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import Accomodate from "./Accomodate";
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+    <Accomodate/>
+);
